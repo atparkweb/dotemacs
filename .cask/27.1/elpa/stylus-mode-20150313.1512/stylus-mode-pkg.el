f623c0ad7dda7521695485a8aabe35b73fc93898
Andrew Park <atpark333@gmail.com>
2020-11-10 14:40:05 +0900

    Remove cask dependencies from VCS

--- .cask/27.1/elpa/stylus-mode-20150313.1512/stylus-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /home/atpark333/.emacs.d/.cask/27.1/elpa/stylus-mode-20150313.1512/stylus-mode.el  -*- no-byte-compile: t -*-
-(define-package "stylus-mode" "20150313.1512" "Major mode for editing .jade files" '((sws-mode "0")) :commit "4dbde92542fc7ad61df38776980905a4721d642e" :authors '(("Brian M. Carlson and other contributors")) :maintainer '("Brian M. Carlson and other contributors") :url "https://github.com/brianc/jade-mode")
