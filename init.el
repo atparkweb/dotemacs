66a2e1adaba9870e47529ccc02b0ec88c7696d6c
Andrew Park <atpark333@gmail.com>
2021-02-20 12:21:39 +0900

    Add prolog config

--- init.el
+++ init.el
@@ -777,6 +777,14 @@
                                          (interactive-haskell-mode)
                                          (turn-on-haskell-indentation)))))
 
+;; Prolog
+(autoload 'run-prolog "prolog" "Start a Prolog sub-process." t)
+(autoload 'prolog-mode "prolog" "Major mode for editing Prolog programs." t)
+(autoload 'mercury-mode "prolog" "Major mode for editing Mercury programs." t)
+(setq prolog-system 'swi)
+(setq auto-mode-alist (append '(("\\.pl$" . prolog-mode)
+                                ("\\.m$" . mercury-mode))
+                               auto-mode-alist))
 ;; Ledger-mode
 ;; ================================================================================
 (use-package ledger-mode
